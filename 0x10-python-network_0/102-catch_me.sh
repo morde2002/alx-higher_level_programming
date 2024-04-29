b4bf1a027fc9e53f9cb2695d0c792dd790d56b80
morde2002 <mathengemordecai@gmail.com>
2024-04-28 23:50:30 -0700

    102-catch_me.sh

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-# sends a JSON POST request to a URL passed as the first argument,
-curl -sX PUT 0.0.0.0:5000/catch_me -L -F 'user_id=98' -H "Origin: You got me!"
+# Sends a request to 0.0.0.0:5000/catch_me causing the server to respond with "You got me!"
+
+curl -sX PUT 0.0.0.0:5000/catch_me -d "user_id=98" -H "Origin: HolbertonSchool"
